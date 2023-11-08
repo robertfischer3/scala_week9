03da6ced2a69f885da941c364bb067786014e0f2
Robert Fischer <robert@oakleyrain.com>
2023-11-07 21:39:08 -0600

    Initial Check in

--- src/main/scala/org/cscie88c/week9/SparkRDDApplication.scala
+++ src/main/scala/org/cscie88c/week9/SparkRDDApplication.scala
@@ -6,9 +6,10 @@ import org.cscie88c.config.{ConfigUtils}
 import org.cscie88c.utils.{SparkUtils}
 import org.apache.spark.rdd.{RDD}
 import pureconfig.generic.auto._
+import pureconfig._
 
 // write case class below
-case class SparkRDDConfig(name: String, masterUrl: String)
+case class SparkRDDConfig(name: String, masterUrl: String, transactionFile: String)
 
 // run with: sbt "runMain org.cscie88c.week9.SparkRDDApplication"
 object SparkRDDApplication {
@@ -16,15 +17,27 @@ object SparkRDDApplication {
   // application entry point
   def main(args: Array[String]): Unit = {
     implicit val conf:SparkRDDConfig = readConfig()                         // 1. read configuration
+    println(conf)
+    
     val spark = SparkUtils.sparkSession(conf.name, conf.masterUrl)          // 2. initialize spark session
     val rddLines = loadData(spark)                                          // 3.load data
-    val rddTransactions = lineToTransactions(rddLines)                      // 4. convert lines to transaction objects
+    /* val rddTransactions = lineToTransactions(rddLines)                      // 4. convert lines to transaction objects
     val yearlyTransactionsRDD = transactionsAmountsByYear(rddTransactions)  // 5. transform data
     printTransactionsAmountsByYear(yearlyTransactionsRDD)                   // 6. print results
     spark.stop()                                                            // 7. stop spark cluster
+    */
   }
 
-  def readConfig(): SparkRDDConfig = ???
+  def readConfig(): SparkRDDConfig = {
+
+  //pureconfig.loadConfigOrThrow[SparkRDDConfig]
+
+  val conf: SparkRDDConfig = ConfigSource.default
+      .at("org.cscie88c.spark-rdd-application")
+      .loadOrThrow[SparkRDDConfig]
+  
+  conf
+}
 
   def loadData(spark: SparkSession)(implicit conf: SparkRDDConfig): RDD[String] = ???
 
