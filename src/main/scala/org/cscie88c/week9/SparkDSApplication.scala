ea83614db79d2bd641fa50496806bef88d37d9b6
Robert Fischer <robert@oakleyrain.com>
2023-11-10 20:24:33 -0600

    Backup 10112023

--- src/main/scala/org/cscie88c/week9/SparkDSApplication.scala
+++ src/main/scala/org/cscie88c/week9/SparkDSApplication.scala
@@ -4,7 +4,8 @@ import org.apache.spark.sql.SparkSession
 import com.typesafe.scalalogging.{LazyLogging}
 import org.cscie88c.config.{ConfigUtils}
 import org.cscie88c.utils.{SparkUtils}
-import org.apache.spark.sql.{Dataset}
+import org.apache.spark.sql.{SparkSession, Dataset, Encoder, Encoders}
+import org.apache.spark.sql.expressions.Aggregator
 
 import pureconfig.generic.auto._
 import pureconfig._
@@ -43,32 +44,52 @@ object SparkDSApplication {
   )(implicit conf: SparkDSConfig): Dataset[CustomerTransaction] = {
     import spark.implicits._
 
-    spark.read
+    // Read the CSV file into a Dataset[RawTransaction]
+    val rawTransactions = spark.read
       .option("header", "true")
+      .option("inferSchema", "true")
       .csv(conf.transactionFile)
-      .as[CustomerTransaction]
+      .as[RawTransaction]
+
+    // Convert RawTransaction to CustomerTransaction
+    val customerTransactions =
+      rawTransactions.map(raw => CustomerTransaction(raw))
+
+    customerTransactions
   }
 
   def transactionTotalsByCategory(
       spark: SparkSession,
       transactions: Dataset[CustomerTransaction]
   ): Dataset[(String, Double)] = {
-
     import spark.implicits._
 
-    transactions
-      .as[CustomerTransaction]
-      .map(t => (getCategory(t), t.transactionAmount))
-      .groupByKey(_._1)
-      .reduceGroups((c1, c2) => (c1._1, c1._2 + c2._2))
-      .map { case (category, total) => (category, total) }
+    val sumAggregator = new Aggregator[CustomerTransaction, Double, Double] {
+      def zero: Double = 0.0
+      def reduce(b: Double, a: CustomerTransaction): Double =
+        b + a.transactionAmount
+      def merge(b1: Double, b2: Double): Double = b1 + b2
+      def finish(reduction: Double): Double = reduction
+      override def bufferEncoder: Encoder[Double] = Encoders.scalaDouble
+      override def outputEncoder: Encoder[Double] = Encoders.scalaDouble
+    }.toColumn
 
+    // Group by category and calculate total
+    val result = transactions
+      .groupByKey(t => if (t.transactionAmount > 80) "High" else "Standard")
+      .agg(sumAggregator)
+
+    result
   }
 
-  def getCategory(t: CustomerTransaction): String = {
-    if (t.transactionAmount > 80) "High" else "Standard"
+  def printTransactionTotalsByCategory(ds: Dataset[(String, Double)]): Unit = {
+    // Collect the dataset to the driver node as an Array
+    val categoryTotals = ds.collect()
+
+    // Iterate through the array and print each category and its total
+    categoryTotals.foreach { case (category, total) =>
+      println(s"Category: $category, Total: $total")
+    }
   }
 
-  def printTransactionTotalsByCategory(ds: Dataset[(String, Double)]): Unit =
-    ???
 }
